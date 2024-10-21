80f81500df48b96225beaa83cbc099f9da80dcb1
Faisal-Abdo <faisalalbusaidy2015@gmail.com>
2024-10-21 09:52:53 +0400

    Added B takes A's value Question

--- BtakesAValue.java
+++ BtakesAValue.java
@@ -1,2 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class BtakesAValue {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("a", "aaa");
+        map.put("b", "xyz");
+        map.put("c", "ccc");
+        modifyAandB(map);
+    }
+
+    public static Map<String, String> modifyAandB(Map<String, String> randomMap) {
+        if (randomMap.containsKey("a")) {
+            randomMap.put("b", randomMap.get("a"));
+            randomMap.replace("a", "");
+        }
+        System.out.println(randomMap);
+        return randomMap;
+    }
 }
